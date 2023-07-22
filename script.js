6955135279880bb1483c6e233fa4331c4450a7ae
Mac <mact0003@gmail.com>
2023-07-22 17:11:56 -0400

    added slider to control animation speed

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+
 class Layer {
 	constructor(image, speedModifier){
 		this.x = 0;
