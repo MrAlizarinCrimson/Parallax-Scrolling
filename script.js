7ec685a9d7996b9ba8365846d09db31f4ee40dde
Mac <mact0003@gmail.com>
2023-07-22 16:59:16 -0400

    added gameobjects for transition

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+let gameSpeed = 4;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -16,19 +16,19 @@ const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
 class Layer {
-	constructor(image, speedoModifier){
+	constructor(image, speedModifier){
 		this.x = 0;
 		this.y = 0;
 		this.width = 2400;
 		this.height = 700;
 		this.x2 = this.width;
 		this.image = image;
-		this.speedoModifier = speedoModifier;
-		this.speed = gameSpeed * this.speedoModifier;
+		this.speedModifier = speedModifier;
+		this.speed = gameSpeed * this.speedModifier;
 	}
 	update(){
 		//to move layers horizontally by changing .x & .y to reset
-		this.speed = gameSpeed * this.speedoModifier;
+		this.speed = gameSpeed * this.speedModifier;
 		if (this.x <= this.width){
 			this.x = this.width + this.x2 - this.speed;
 		}
@@ -47,16 +47,25 @@ class Layer {
 	}
 }
 
-const layer4 = new Layer()
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
 
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
 	//this will allow to clear old canvas and take only current in transition
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	gameObjects.forEach(object => {
+		object.update();
+		object.draw();
+	});
 
-	
-	
 	requestAnimationFrame(animate);
-}
+
+		
+};
 animate();
 
